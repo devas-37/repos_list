0f77fbcdb2d078cc1a0d850fe88162f4400c4d4d
allayor <htecgf@gmail.com>
2021-10-02 12:19:43 +0500

    to be Continue

---
+++ src/views/Search/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logo">
+    <img src="../../assets/logo-github.svg" alt="GitHub logo"/>
+  </div>
+  <div class="search">
+    <input class="form-control">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "Home",
+};
+</script>
