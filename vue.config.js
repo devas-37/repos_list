2ebeb1dbbf818a332e9ef47b43aee63db49e24e6
allayor <htecgf@gmail.com>
2021-10-02 10:12:33 +0500

    Readd

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "ru",
+      localeDir: "src",
+      enableLegacy: true,
+      runtimeOnly: false,
+      compositionOnly: true,
+      fullInstall: true,
+    },
+  },
+};
